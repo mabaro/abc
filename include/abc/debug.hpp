7021c39967a32020378f85c78d0198e5df98f1e7
marcos <markisbcn@gmail.com>
2023-10-10 00:05:47 +0200

    reformatted

--- include/abc/debug.hpp
+++ include/abc/debug.hpp
@@ -13,137 +13,133 @@
 #include <iostream>
 
 #if defined(ABC_FORCE_DEBUG) && defined(ABC_RETAIL)
-#  error "ABC_FORCE_DEBUG and ABC_RETAIL are incompatible!"
+#error "ABC_FORCE_DEBUG and ABC_RETAIL are incompatible!"
 #endif
 
 #if defined(ABC_DEBUG)
-#  error "Do not pass ABC_DEBUG to the compiler. If you want to force debug, pass ABC_FORCE_DEBUG"
+#error "Do not pass ABC_DEBUG to the compiler. If you want to force debug, pass ABC_FORCE_DEBUG"
 #endif
 
 #ifndef ABC_RELEASE_CHECKED
-#   if !defined(NDEBUG) || defined(ABC_FORCE_DEBUG)
-#      define ABC_DEBUG
-#   else
-#      define ABC_RELEASE
-#   endif
+#if !defined(NDEBUG) || defined(ABC_FORCE_DEBUG)
+#define ABC_DEBUG
+#else
+#define ABC_RELEASE
+#endif
 #endif
 
 #if defined(ABC_DEBUG) || defined(ABC_RELEASE_CHECKED)
-#  if defined(ABC_PLATFORM_WINDOWS_FAMILY)
-#    define ABC_BREAK() \
-      __debugbreak()  // better than assert(0) as it will break on this exact line instead of deep
-                      // in the assert function.
-#  elif defined(ABC_PLATFORM_OSX_FAMILY) || defined(ABC_PLATFORM_LINUX_FAMILY)
-#    include <signal.h>
-#    define ABC_BREAK() raise(SIGTRAP)
-#  elif defined(ABC_PLATFORM_ANDROID_FAMILY)
-#    include <signal.h>
-#    define ABC_BREAK() raise(SIGSTOP)
-#  else
-#    error "Unsupported platform"
-#  endif
+#if defined(ABC_PLATFORM_WINDOWS_FAMILY)
+#define ABC_BREAK() \
+    __debugbreak()   // better than assert(0) as it will break on this exact line instead of deep
+                     // in the assert function.
+#elif defined(ABC_PLATFORM_OSX_FAMILY) || defined(ABC_PLATFORM_LINUX_FAMILY)
+#include <signal.h>
+#define ABC_BREAK() raise(SIGTRAP)
+#elif defined(ABC_PLATFORM_ANDROID_FAMILY)
+#include <signal.h>
+#define ABC_BREAK() raise(SIGSTOP)
 #else
-#  define ABC_BREAK()
+#error "Unsupported platform"
+#endif
+#else
+#define ABC_BREAK()
 #endif
 
-namespace abc
-{
-namespace assert
-{
-namespace detail
-{
-enum class response { BREAK, CONTINUE };
+namespace abc {
+namespace assert {
+namespace detail {
+enum class response {
+    BREAK,
+    CONTINUE
+};
 response do_assert(const char* condition, const char* file, int line, const char* message);
 
-template<typename PARAM>
-struct is_param_string                             { static constexpr bool value = false; };
-template<> struct is_param_string<char>            { static constexpr bool value = true; };
-template<size_t N> struct is_param_string<char[N]> { static constexpr bool value = true; };
+template <typename PARAM> struct is_param_string {
+    static constexpr bool value = false;
+};
+template <> struct is_param_string<char> {
+    static constexpr bool value = true;
+};
+template <size_t N> struct is_param_string<char[N]> {
+    static constexpr bool value = true;
+};
 
-}  // namespace detail
-}  // namespace assert
-}  // namespace abc
+}   // namespace detail
+}   // namespace assert
+}   // namespace abc
 
 /// Log
-#define ABC_LOG_ERROR_IMPL(MSG, ...)                                  \
-  do {                                                                \
-    std::cerr << abc::format("[ERROR][{}:{}] {}", __FILE__, __LINE__, \
-                             abc::format(MSG, __VA_ARGS__))           \
-              << std::endl;                                           \
-  } while (0)
+#define ABC_LOG_ERROR_IMPL(MSG, ...)                                                                                   \
+    do {                                                                                                               \
+        std::cerr << abc::format("[ERROR][{}:{}] {}", __FILE__, __LINE__, abc::format(MSG, __VA_ARGS__)) << std::endl; \
+    } while (0)
 
-#define ABC_LOG_WARNING_IMPL(MSG, ...)                                  \
-  do {                                                                  \
-    std::cerr << abc::format("[WARNING][{}:{}] {}", __FILE__, __LINE__, \
-                             abc::format(MSG, __VA_ARGS__))             \
-              << std::endl;                                             \
-  } while (0)
+#define ABC_LOG_WARNING_IMPL(MSG, ...)                                                                     \
+    do {                                                                                                   \
+        std::cerr << abc::format("[WARNING][{}:{}] {}", __FILE__, __LINE__, abc::format(MSG, __VA_ARGS__)) \
+                  << std::endl;                                                                            \
+    } while (0)
 
-#define ABC_LOG_DEBUG_IMPL(MSG, ...)                                  \
-  do {                                                                \
-    std::cerr << abc::format("[DEBUG][{}:{}] {}", __FILE__, __LINE__, \
-                             abc::format(MSG, __VA_ARGS__))           \
-              << std::endl;                                           \
-  } while (0)
+#define ABC_LOG_DEBUG_IMPL(MSG, ...)                                                                                   \
+    do {                                                                                                               \
+        std::cerr << abc::format("[DEBUG][{}:{}] {}", __FILE__, __LINE__, abc::format(MSG, __VA_ARGS__)) << std::endl; \
+    } while (0)
 
-#define ABC_LOG_INFO_IMPL(MSG, ...)                                  \
-  do {                                                               \
-    std::cerr << abc::format("[INFO][{}:{}] {}", __FILE__, __LINE__, \
-                             abc::format(MSG, __VA_ARGS__))          \
-              << std::endl;                                          \
-  } while (0)
+#define ABC_LOG_INFO_IMPL(MSG, ...)                                                                                   \
+    do {                                                                                                              \
+        std::cerr << abc::format("[INFO][{}:{}] {}", __FILE__, __LINE__, abc::format(MSG, __VA_ARGS__)) << std::endl; \
+    } while (0)
 
 /// Assertions
-#define ABC_ASSERT_IMPL(condition, ...)                                                   \
-  do {                                                                                    \
-  	const auto& __cond_result__ = (condition);																						\
-    ABC_UNUSED(__cond_result__);																											    \
-	  static_assert(!abc::assert::detail::is_param_string<decltype(__cond_result__)>::value,\
-      "Condition is a string, probably swapped parameter order");                         \
-    if (!(condition)) {                                                                   \
-      abc::string msg = abc::format<abc::string>(__VA_ARGS__);                            \
-      if (abc::assert::detail::do_assert(#condition, __FILE__, __LINE__, msg.data()) ==   \
-          abc::assert::detail::response::BREAK) {                                         \
-        ABC_BREAK();                                                                      \
-      }                                                                                   \
-    }                                                                                     \
-  } while (false)
+#define ABC_ASSERT_IMPL(condition, ...)                                                        \
+    do {                                                                                       \
+        const auto& __cond_result__ = (condition);                                             \
+        static_assert(!abc::assert::detail::is_param_string<decltype(__cond_result__)>::value, \
+            "Condition is a string, probably swapped parameter order");                        \
+        if (!(condition)) {                                                                    \
+            abc::string msg = abc::format<abc::string>(__VA_ARGS__);                           \
+            if (abc::assert::detail::do_assert(#condition, __FILE__, __LINE__, msg.data())     \
+                == abc::assert::detail::response::BREAK) {                                     \
+                ABC_BREAK();                                                                   \
+            }                                                                                  \
+        }                                                                                      \
+    } while (false)
 
-#define ABC_FAIL_IMPL(...) \
-  do {                                                                                  \
-      abc::string msg = abc::format<abc::string>(__VA_ARGS__);                          \
-      std::cerr << "FAIL: " << msg << "[" << __FILE__ << ":" << __LINE__  << "]"        \
-        << std::endl;                                                                   \
-      ABC_BREAK();                                                                      \
-  } while (false)
+#define ABC_FAIL_IMPL(...)                                                                      \
+    do {                                                                                        \
+        abc::string msg = abc::format<abc::string>(__VA_ARGS__);                                \
+        std::cerr << "FAIL: " << msg << "[" << __FILE__ << ":" << __LINE__ << "]" << std::endl; \
+        ABC_BREAK();                                                                            \
+    } while (false)
 
 ///////////////////////////////////////////////////////////////////////////////
 
 #if defined(ABC_DEBUG) || defined(ABC_RELEASE_CHECKED)
-#  define ABC_ASSERT(_condition_, ...)          ABC_ASSERT_IMPL(_condition_, ##__VA_ARGS__)
-#  define ABC_FAIL                              ABC_FAIL_IMPL
-#  define ABC_ASSERT_RELEASE(_condition_, ...)  ABC_ASSERT_IMPL(_condition_, ##__VA_ARGS__)
-#  define ABC_FAIL_RELEASE                      ABC_FAIL_IMPL
+#define ABC_ASSERT(_condition_, ...)         ABC_ASSERT_IMPL(_condition_, ##__VA_ARGS__)
+#define ABC_FAIL                             ABC_FAIL_IMPL
+#define ABC_ASSERT_RELEASE(_condition_, ...) ABC_ASSERT_IMPL(_condition_, ##__VA_ARGS__)
+#define ABC_FAIL_RELEASE                     ABC_FAIL_IMPL
 
-#  define ABC_LOG_ERROR(MSG, ...)               ABC_LOG_ERROR_IMPL(MSG, ##__VA_ARGS__)
-#  define ABC_LOG_WARNING(MSG, ...)             ABC_LOG_WARNING_IMPL(MSG, ##__VA_ARGS__)
-#  define ABC_LOG_INFO(MSG, ...)                ABC_LOG_INFO_IMPL(MSG, ##__VA_ARGS__)
-#  define ABC_LOG_DEBUG(MSG, ...)               ABC_LOG_DEBUG_IMPL(MSG, ##__VA_ARGS__)
+#define ABC_LOG_ERROR(MSG, ...)   ABC_LOG_ERROR_IMPL(MSG, ##__VA_ARGS__)
+#define ABC_LOG_WARNING(MSG, ...) ABC_LOG_WARNING_IMPL(MSG, ##__VA_ARGS__)
+#define ABC_LOG_INFO(MSG, ...)    ABC_LOG_INFO_IMPL(MSG, ##__VA_ARGS__)
+#define ABC_LOG_DEBUG(MSG, ...)   ABC_LOG_DEBUG_IMPL(MSG, ##__VA_ARGS__)
 #else
-#  define ABC_ASSERT(...)
-#  define ABC_FAIL(...)
-#  define ABC_ASSERT_RELEASE(_condition_, ...)  ABC_ASSERT_IMPL(_condition_, ##__VA_ARGS__)
-#  define ABC_FAIL_RELEASE                      ABC_FAIL_IMPL
+#define ABC_ASSERT(...)
+#define ABC_FAIL(...)
+#define ABC_ASSERT_RELEASE(_condition_, ...) ABC_ASSERT_IMPL(_condition_, ##__VA_ARGS__)
+#define ABC_FAIL_RELEASE                     ABC_FAIL_IMPL
 
-#  define ABC_LOG_ERROR(MSG, ...)              ABC_LOG_ERROR_IMPL(MSG, ##__VA_ARGS__)
-#  define ABC_LOG_WARNING(MSG, ...)            ABC_LOG_WARNING_IMPL(MSG, ##__VA_ARGS__)
-#  define ABC_LOG_INFO(MSG, ...)               ABC_LOG_INFO_IMPL(MSG, ##__VA_ARGS__)
-#  define ABC_LOG_DEBUG(...)
+#define ABC_LOG_ERROR(MSG, ...)   ABC_LOG_ERROR_IMPL(MSG, ##__VA_ARGS__)
+#define ABC_LOG_WARNING(MSG, ...) ABC_LOG_WARNING_IMPL(MSG, ##__VA_ARGS__)
+#define ABC_LOG_INFO(MSG, ...)    ABC_LOG_INFO_IMPL(MSG, ##__VA_ARGS__)
+#define ABC_LOG_DEBUG(...)
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "abc/string.hpp"
 #include "abc/format.hpp"
+#include "abc/string.hpp"
--- include/abc/tagged_type.hpp
+++ include/abc/tagged_type.hpp
@@ -3,96 +3,93 @@
 #include <iostream>
 #include <type_traits>
 
-namespace abc
-{
-    
-template <typename T, typename TTag, int64_t DefaultValue>
-class tagged_type
-{
-    static_assert(std::is_trivial<T>::value, "Only trivial types are supported");
-    T m_value;
+namespace abc {
 
-public:
-    using value_t = T;
-    using this_t  = tagged_type<T, TTag, DefaultValue>;
+    template <typename T, typename TTag, int64_t DefaultValue>
+    class tagged_type {
+        static_assert(std::is_trivial<T>::value, "Only trivial types are supported");
+        T m_value;
 
-    static constexpr T default_value = DefaultValue;
+    public:
+        using value_t = T;
+        using this_t = tagged_type<T, TTag, DefaultValue>;
 
-public:
-    tagged_type(uninitialized_t) {}
-    tagged_type() : m_value(default_value) {}
-    explicit tagged_type(const T i_value) : m_value(i_value) {}
-    tagged_type(const this_t& i_other) : m_value(i_other.m_value) {}
-    tagged_type(this_t&& i_other) : m_value(std::move(i_other.m_value)) {}
-    ~tagged_type() = default;
+        static constexpr T default_value = DefaultValue;
 
-    this_t& operator=(const this_t& i_other) noexcept
-    {
-        m_value = i_other.m_value;
-        return *this;
-    }
-    this_t& operator=(this_t&& i_other) noexcept
-    {
-        m_value = std::move(i_other.m_value);
-        return *this;
-    }
+    public:
+        tagged_type(uninitialized_t) {}
+        tagged_type() : m_value(default_value) {}
+        explicit tagged_type(const T i_value) : m_value(i_value) {}
+        tagged_type(const this_t& i_other) : m_value(i_other.m_value) {}
+        tagged_type(this_t&& i_other) : m_value(std::move(i_other.m_value)) {}
+        ~tagged_type() = default;
 
-    explicit constexpr operator T&() noexcept { return m_value; }
-    explicit constexpr operator const T&() const noexcept { return m_value; }
+        this_t& operator=(const this_t& i_other) noexcept
+        {
+            m_value = i_other.m_value;
+            return *this;
+        }
+        this_t& operator=(this_t&& i_other) noexcept
+        {
+            m_value = std::move(i_other.m_value);
+            return *this;
+        }
 
-    constexpr const T& value() const noexcept { return m_value; }
-    T&                 value() noexcept { return m_value; }
+        explicit constexpr operator T&() noexcept { return m_value; }
+        explicit constexpr operator const T&() const noexcept { return m_value; }
 
-    bool operator==(const this_t& other) const noexcept { return m_value == other.m_value; }
-    bool operator!=(const this_t& other) const noexcept { return !operator==(other); }
+        constexpr const T& value() const noexcept { return m_value; }
+        T& value() noexcept { return m_value; }
 
-    using enable_for_arithmetic = std::enable_if<std::is_arithmetic<T>::value, this_t>;
+        bool operator==(const this_t& other) const noexcept { return m_value == other.m_value; }
+        bool operator!=(const this_t& other) const noexcept { return !operator==(other); }
 
-    typename enable_for_arithmetic::type operator+(const this_t& other) const
-    {
-        return this_t(m_value + other.m_value);
-    }
-    typename enable_for_arithmetic::type operator-(const this_t& other) const
-    {
-        return this_t(m_value - other.m_value);
-    }
-    typename enable_for_arithmetic::type operator*(const this_t& other) const
-    {
-        return this_t(m_value * other.m_value);
-    }
-    typename enable_for_arithmetic::type operator/(const this_t& other) const
-    {
-        return this_t(m_value / other.m_value);
-    }
+        using enable_for_arithmetic = std::enable_if<std::is_arithmetic<T>::value, this_t>;
 
-    typename enable_for_arithmetic::type& operator+=(const this_t& other)
-    {
-        m_value += other.m_value;
-        return *this;
-    }
-    typename enable_for_arithmetic::type& operator-=(const this_t& other)
-    {
-        m_value -= other.m_value;
-        return *this;
-    }
-    typename enable_for_arithmetic::type& operator*=(const this_t& other)
-    {
-        m_value *= other.m_value;
-        return *this;
-    }
-    typename enable_for_arithmetic::type& operator/=(const this_t& other)
-    {
-        m_value /= other.m_value;
-        return *this;
-    }
-};
+        typename enable_for_arithmetic::type operator+(const this_t& other) const
+        {
+            return this_t(m_value + other.m_value);
+        }
+        typename enable_for_arithmetic::type operator-(const this_t& other) const
+        {
+            return this_t(m_value - other.m_value);
+        }
+        typename enable_for_arithmetic::type operator*(const this_t& other) const
+        {
+            return this_t(m_value * other.m_value);
+        }
+        typename enable_for_arithmetic::type operator/(const this_t& other) const
+        {
+            return this_t(m_value / other.m_value);
+        }
 
-}  // namespace abc
+        typename enable_for_arithmetic::type& operator+=(const this_t& other)
+        {
+            m_value += other.m_value;
+            return *this;
+        }
+        typename enable_for_arithmetic::type& operator-=(const this_t& other)
+        {
+            m_value -= other.m_value;
+            return *this;
+        }
+        typename enable_for_arithmetic::type& operator*=(const this_t& other)
+        {
+            m_value *= other.m_value;
+            return *this;
+        }
+        typename enable_for_arithmetic::type& operator/=(const this_t& other)
+        {
+            m_value /= other.m_value;
+            return *this;
+        }
+    };
 
-//#define ABC_NAMED_TYPE_INTEGRAL(NAME, TYPE, DEFAULT_VALUE)     \
-//struct ##NAME_traits                                           \
-//{                                                              \
-//    static const TYPE default_value;                           \
-//};                                                             \
-//const TYPE ##NAME_traits::default_value = DEFAULT_VALUE;       \
-//using NAME = abc::tagged_type<TYPE, struct ##NAME_tag, ##NAME_traits>;
+} // namespace abc
+
+#define ABC_NAMED_TYPE_INTEGRAL(NAME, TYPE, DEFAULT_VALUE)  \
+    struct##NAME_traits {                                   \
+        static const TYPE default_value;                    \
+    };                                                      \
+    const TYPE##NAME_traits::default_value = DEFAULT_VALUE; \
+    using NAME = abc::tagged_type<TYPE, struct##NAME_tag, ##NAME_traits>;
